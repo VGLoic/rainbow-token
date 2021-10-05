634ef3b3bb03b5028af81b3a6c2b1fdd6305b107
VGLoic <loic.vincent-genod@consensys.net>
2021-10-05 18:01:07 +0200

    finish metamask connection test

--- packages/client/src/__tests__/connection.test.tsx
+++ packages/client/src/__tests__/connection.test.tsx
@@ -4,7 +4,22 @@ import userEvent from "@testing-library/user-event";
 import { setupEthTesting } from "eth-testing";
 
 describe("Connection to application", () => {
+  describe("when MetaMask is unavailable", () => {
+    test("it should disabled the connect with MetaMask button", async () => {
+      render(<App />);
+
+      await waitFor(() => {
+        expect(
+          screen.getByText(/Connect with your favorite wallet/i)
+        ).toBeInTheDocument();
+      });
+
+      expect(screen.getByRole("button", { name: /metamask/i })).toBeDisabled();
+    });
+  });
+
   describe("when MetaMask is available", () => {
+    const address = "0xf61B443A155b07D2b2cAeA2d99715dC84E839EEf";
     const { provider, testingUtils } = setupEthTesting({
       providerType: "MetaMask",
     });
@@ -17,9 +32,11 @@ describe("Connection to application", () => {
       (global.window as any).ethereum = undefined;
     });
 
-    test("it should allow the user to connect successfully", async () => {
-      const address = "0xf61B443A155b07D2b2cAeA2d99715dC84E839EEf";
+    afterEach(() => {
+      testingUtils.clearAllMocks();
+    });
 
+    test("it should allow the user to connect successfully", async () => {
       testingUtils.mockChainId("0x1");
       testingUtils.mockAccounts([]);
 
@@ -33,13 +50,27 @@ describe("Connection to application", () => {
         ).toBeInTheDocument();
       });
 
-      userEvent.click(screen.getByRole("button", { name: /metamask/i }));
+      const connectWithMetaMaskButton = screen.getByRole("button", {
+        name: /metamask/i,
+      });
 
-      await waitForElementToBeRemoved(
-        screen.getByRole("button", { name: /metamask/i })
-      );
+      userEvent.click(connectWithMetaMaskButton);
+      expect(connectWithMetaMaskButton).toBeDisabled();
+
+      await waitForElementToBeRemoved(connectWithMetaMaskButton);
 
       expect(screen.getByText(/account/i)).toBeInTheDocument();
+    });
+
+    test("it should go directly to the connected app when MetaMask is already connected", async () => {
+      testingUtils.mockAccounts([address]);
+      testingUtils.mockChainId("0x1");
+
+      render(<App />);
+
+      await waitFor(() => {
+        expect(screen.getByText(/account/i)).toBeInTheDocument();
+      });
     });
   });
 });
